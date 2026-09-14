ff25a0391ddf53507e395d395f1ea5d3773b1893
Rustam <rustam@MacBook-Pro-admin.local>
2026-09-14 21:47:38 +0500

    Use environment secret key

--- drons/settings.py
+++ drons/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wf9nl8%n2)j&@%54*y75*t#ds0c3vuk=@7-ne+)krxys&#ob9r'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
